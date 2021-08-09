f16f9515aadfaa912388bbe0f57ac66ac95370fb
Aditee Singh <singhaditee33@gmail.com>
2021-08-10 03:34:50 +0530

    Projects added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
@@ -11,10 +11,32 @@ const Projects = () => (
       Projects
     </SectionTitle>
     <GridContainer>
-      {[0,1,2].map((project)=>
-      <div>
-        {project}
-      </div>)}
+      {projects.map(({id , title, description, image, tags, visit, source})=>
+      <BlogCard key={id}>
+        <Img src={image} />
+        <TitleContent>
+          <HeaderThree title>{title}</HeaderThree>
+          <Hr />
+        </TitleContent>
+        <CardInfo>
+           {description}
+        </CardInfo>
+        <div>
+          <TitleContent  style={{marginTop: "15px"}}>
+            Technologies
+          </TitleContent>
+          <TagList>
+            {tags.map((tag , i)=>(
+              <Tag key={i}>{tag}</Tag>
+            ))}
+          </TagList>
+        </div>
+        <UtilityList>
+          <ExternalLinks href={source}>Code</ExternalLinks>
+          {visit.length!=0? <ExternalLinks href={visit}>Demo</ExternalLinks> : null}
+        </UtilityList>
+      </BlogCard>
+      )}
     </GridContainer>
   </Section>
 );
---
+++ src/constants/constants.js
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    title: 'Giphy Clone',
+    description: "This Project fetches and renders GIFs based on the users search using the Giphy API",
+    image: '/images/download.png',
+    tags: ['HTML' , 'Javascript' , 'Jquery' , 'CSS Grids'],
+    source: 'https://github.com/aditee-singh/giphy',
+    visit: 'https://aditee.live/giphy3.html',
+    id: 0,
+  },
+  {
+    title: 'Travel Website',
+    description:"It is a dummy website for a travel agency built to familiarize myself with the basics of React and get better understanding of React hooks.",
+    image: '/images/2.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://github.com/aditee-singh/Travel_Website',
+    visit: '',
+    id: 1,
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Founded JavaScript Mastery', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
